d078fc1895db2d04892a14fd97cfc1641fa17a83
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-08 09:28:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,4 +2,15 @@ class Songs
   @@count=0
   attr_accessor :name,:artist, :genre
   
+  def initialize(name,artist,genre)
+    @name=name
+    @artist=artist
+    @genre=genre
+    @@count+=1
+  end
+  
+  def self.count
+    @@count
+  end
+  
   
